5537491dcd3d8766d80b922507c6904f6cc5cae2
jxck <block.rxckin.beats@gmail.com>
2020-10-18 20:27:04 +0900

    [book] up format.sh

--- books/format.sh
+++ books/format.sh
@@ -1,8 +1,23 @@
+spacer    **/*.md
 format.rb **/*.md
 
-git grep '説明'
-git grep 'です'
-git grep 'ます'
-git grep 'しょう'
-git grep '章'
-git grep 'セクション'
+git grep '説明'             | cat
+git grep 'です'             | grep -v 加筆修正したものです
+git grep --no-color 'ます'  | grep -v 成り済ます
+git grep 'しょう'           | cat
+git grep 'セクション'       | cat
+git grep 'Wifi'             | cat
+git grep -i 'registerable'  | cat
+git grep -i 'doamin'        | cat
+git grep -i 'fetc '         | cat
+git grep -i 'cros '         | cat
+git grep -i 'scirpt'        | cat
+git grep -i 'scritp'        | cat
+git grep -i 'requst'        | cat
+git grep -i 'titile'        | cat
+git grep -i 'purchse'       | cat
+git grep -i 'statefull'     | cat
+git grep -i 'seasion'       | cat
+git grep -i 'exmaple'       | cat
+git grep -i 'cooki '        | cat
+git grep -i 'navication'    | cat
